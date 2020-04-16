b5be2c93ddfbdc223888d3b48031dc2bd7131870
lulu031100 <lulu031100@gmail>
2020-04-16 21:41:40 +0900

    finished

--- takeout/forms.py
+++ takeout/forms.py
@@ -6,3 +6,7 @@ class ShopCreateForm(forms.ModelForm):
     class Meta:
         model = Shop
         fields = '__all__'
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['address'].widget.attrs.update({'rows':'2'})
+        self.fields['hours'].widget.attrs.update({'rows':'3'})
